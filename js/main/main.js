10164ad4ea0156d396c173666a2a24793d700017
Jeremy P <jeremy21@gmail.com>
2015-11-13 16:39:41 -0800

    end of day

--- js/main/main.js
+++ js/main/main.js
@@ -1,17 +1,22 @@
 $(function() {
+
 var instagridItems='',
 		searchString,
 		searchUrl;
+
 $('button#button').on('click', function(event) {
+	//Main search function
 	event.preventDefault();
+	$('ul.flexcontainer').fadeOut('fast');
+	$('div.loading').fadeIn('fast');
+	//Hides the results, shows loader
+	$('div.flexitem').addClass('magic');
+	//Add class for formatting purposes
 	searchString = $('#search').val().replace(/ /g, '_');
 	searchUrl = 'https://api.instagram.com/v1/tags/'+ searchString + '/media/recent?client_id=fbee9f2910474c4e8375508626b6243e'
-
-	if ($('ul li').length == 20){$('.listitem').remove()};
+	//
+	if ($('ul li').length >= 20){$('.listitem').remove()};
 	// clear any old search results
-	$('ul.flexcontainer').slideUp('fast');
-	$('div.loading').fadeIn('fast');
-
 
 	$.ajax({
 	      method: 'GET',
@@ -19,22 +24,33 @@ $('button#button').on('click', function(event) {
 	      dataType: 'jsonp'
 	    })
 	 .done(function(data) {
-	 // for (var i = 0; i < 20; i++)
+		 	   $('#initialitem').remove();
+
 	 $.each(data.data, function(key, value) {
-	              //  instagridItems += '<li>';
-	               instagridItems = '<li class="listitem"><div class="flexitem"><img class="mainpicture" src="' + value.images.standard_resolution.url + '">'+'<img class="profile-picture" src="' + value.user.profile_picture + '">' + '<div class="userwrap">' + '<span class="username">' + value.user.username + '</span>' + '<span class="social">' + '<i class="fa fa-comments"></i><p>' + value.comments.count + '&nbsp;&nbsp;&nbsp;</p><i class="fa fa-heart"></i><p>' + value.likes.count  + '</p></span>' +'</div>' + '</div></li>';
+	              var insta_length = value.user.username.length;
 
 
-	   $('#initialitem').remove();
-		 $('.flexcontainer').append(instagridItems);
-		 $('div.loading').fadeOut('fast');
-		 $('ul.flexcontainer').slideDown('fast');
+	              instagridItems = '<li class="listitem"><div class="flexitem"><img class="mainpicture" src="' + value.images.standard_resolution.url + '">'+'<img class="profile-picture" src="' + value.user.profile_picture + '">' + '<div class="userwrap">' + '<span class="username" id="'+ key + '">' + value.user.username + '</span>' + '<span class="social">' + '<i class="fa fa-comments"></i><p>' + value.comments.count + '&nbsp;&nbsp;&nbsp;</p><i class="fa fa-heart"></i><p>' + value.likes.count  + '</p></span>' +'</div>' + '</div></li>';
+
+		 					 	$('.flexcontainer').append(instagridItems);
+
+								if (insta_length >= 20){$('#'+ key).addClass('xxxl-name')}
+								else if (insta_length >= 18 && insta_length < 20) { $('#'+ key).addClass('xxl-name') }
+								else if (insta_length >= 13 && insta_length < 18) { $('#'+ key).addClass('xl-name') }
+								else if (insta_length >= 10 && insta_length < 13) { $('#'+ key).addClass('lg-name') };
+
 
 
  });
 
 
 
- });
+ })
+ .fail(function() {
+	$('div.flexitem').removeClass('magic');
+ 	alert( "error" );
+});
+$('div.loading').fadeOut('fast');
+$('ul.flexcontainer').fadeIn('slow');
  });
  });
