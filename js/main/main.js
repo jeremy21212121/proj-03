7b4165393749e5614e0dbed268f8767b01454b77
Jeremy P <jeremy21@gmail.com>
2015-11-17 18:18:56 -0800

    pagination done

--- js/main/main.js
+++ js/main/main.js
@@ -1,93 +1,117 @@
-var instaPage = '';
-
+var instaPage = {};
+var moreClicks = {};
+moreClicks.count=0;
+var adjustedKey;
+//global variables because my js-fu is weak
 $(function() {
 
-var instagridItems='',
+	var instagridItems='',
 		searchString,
 		searchUrl,
 		instaUrl;
 
+	$('button#more').toggle();
 
-function instagridNameFormatter(value, key) {
-	var insta_length = value.user.username.length;
-	if ( $(window).width() >= 784 ){insta_length=insta_length*1.3}
-	else if ( $(window).width() <= 752 ){insta_length=insta_length/1.4};
-	if (insta_length >= 20){$('#'+ key).addClass('xxxl-name')}
-	else if (insta_length >= 18 && insta_length < 20) { $('#'+ key).addClass('xxl-name') }
-	else if (insta_length >= 13 && insta_length < 18) { $('#'+ key).addClass('xl-name') }
-	else if (insta_length >= 10 && insta_length < 13) { $('#'+ key).addClass('lg-name') };
+	function keyAdjuster(key){
+		var countCorrector = moreClicks.count * 12;
+		 adjustedKey = key + countCorrector;
+
+		//Calculates var adjustedKey to allow use of instagridNameFormatter on 'Load More' results
 	}
-//
 
-function getInstagrid(instaUrl) {
-	$.ajax({
-				method: 'GET',
-				url: instaUrl,
-				dataType: 'jsonp'
+	function instagridNameFormatter(value, key) {
+		var insta_length = value.user.username.length;
+		keyAdjuster(key);
+		if ( $(window).width() >= 784 ){insta_length=insta_length*1.3}
+		else if ( $(window).width() <= 752 ){insta_length=insta_length/1.4};
+		if (insta_length >= 20){$('#'+ adjustedKey ).addClass('xxxl-name')}
+		else if (insta_length >= 18 && insta_length < 20) { $('#'+ adjustedKey ).addClass('xxl-name') }
+		else if (insta_length >= 13 && insta_length < 18) { $('#'+ adjustedKey ).addClass('xl-name') }
+		else if (insta_length >= 10 && insta_length < 13) { $('#'+ adjustedKey ).addClass('lg-name') };
+// complicated algorithm for determining username font size :)
+		}
+
+
+	function getInstagrid(instaUrl) {
+		$.ajax({
+					method: 'GET',
+					url: instaUrl,
+					dataType: 'jsonp'
+				})
+				.done(function(data) {
+					instaPage.url = data.pagination.next_url;
+							$('#initialitem').remove();
+				   $.each(data.data, function(key, value) {
+						 				 keyAdjuster(key);
+										 debugger;
+										 instagridItems =
+										 '<li class="listitem"><div class="flexitem"> <a href="#" data-featherlight="' + value.images.standard_resolution.url + '"><img class="mainpicture" src="'
+										 + value.images.standard_resolution.url + '"></a>'
+										 +'<a target="_blank" href="https://www.instagram.com/'+ value.user.username +'">'+'<img class="profile-picture" src="' + value.user.profile_picture + '"></a>'
+										 +'<div class="userwrap">'
+										 +'<span class="username" id="' + adjustedKey + '">' +'<a target="_blank" href="https://www.instagram.com/'+ value.user.username +'">'+ value.user.username + '</a>' + '</span>'
+										 +'<span class="social">' + '<a target="_blank" href="'+ value.link + '">' + '<i class="fa fa-comments"></i>'
+										 +'<p>' + value.comments.count + '&nbsp;&nbsp;&nbsp;</p>'
+										 +'<i class="fa fa-heart"></i><p>' + value.likes.count  + '</p></a></span>' +'</div>' +'</div></li>';
+
+
+										 $('.flexcontainer').append(instagridItems);
+										 instagridNameFormatter(value, key);
+			       });
+
 			})
-			.done(function(data) {
-				instaPage = data.pagination.next_url;
-						$('#initialitem').remove();
-			   $.each(data.data, function(key, value) {
-									 instagridItems =
-									 '<li class="listitem"><div class="flexitem"><img class="mainpicture" src="' + value.images.standard_resolution.url + '">'+'<img class="profile-picture" src="' + value.user.profile_picture + '">' +'<div class="userwrap">' +'<span class="username" id="'+ key + '">' + value.user.username + '</span>' +'<span class="social">' + '<i class="fa fa-comments"></i><p>' + value.comments.count + '&nbsp;&nbsp;&nbsp;</p><i class="fa fa-heart"></i><p>' + value.likes.count  + '</p></span>' +'</div>' +'</div></li>';
+			.fail(function() {
+			 $('div.flexitem').removeClass('magic');
+			 alert( "error" );
+		 })
+
+		  .always(function(){
 
 
-									 $('.flexcontainer').append(instagridItems);
-									 instagridNameFormatter(value, key);
-		       });
-
-		})
-		.fail(function() {
-		 $('div.flexitem').removeClass('magic');
-		 alert( "error" );
-	 })
-
-	  .always(function(){
+		 });
+// gets the data from instagram, formats it and spits out the HTML
+	}
 
 
+	$('button#button').on('click', function(event) {
+		//Main search function
+				event.preventDefault();
+				$('ul.flexcontainer').toggle('fast');
+				$('div.loading').fadeIn('fast');
+				//Hides the results, shows loader
+
+				$('div.flexitem').addClass('magic');
+				//Add class for formatting purposes
+
+				searchString = $('#search').val().replace(/ /g, '_');
+				searchUrl = 'https://api.instagram.com/v1/tags/'+ searchString + '/media/recent?client_id=fbee9f2910474c4e8375508626b6243e&count=12'
+				//
+				if ($('ul li').length >= 20){$('.listitem').remove()};
+				// clear any old search results
+				getInstagrid(searchUrl);
+
+
+				$('div.loading').fadeOut('fast');
+
+				$('button#more').fadeIn('slow');
+				$('ul.flexcontainer').fadeIn('slow');
 	 });
 
-}
-//Closing getInstagrid
-
-$('button#button').on('click', function(event) {
-	//Main search function
-	event.preventDefault();
-
-			$('ul.flexcontainer').fadeOut('fast');
-			$('div.loading').fadeIn('fast');
-			//Hides the results, shows loader
-			$('div.flexitem').addClass('magic');
-			//Add class for formatting purposes
-			searchString = $('#search').val().replace(/ /g, '_');
-			searchUrl = 'https://api.instagram.com/v1/tags/'+ searchString + '/media/recent?client_id=fbee9f2910474c4e8375508626b6243e'
-			//
-			if ($('ul li').length >= 20){$('.listitem').remove()};
-			// clear any old search results
-			getInstagrid(searchUrl);
 
 
-			$('div.loading').fadeOut('fast');
-			$('ul.flexcontainer').fadeIn('slow');
-
- });
-
- $('button#more').on('click', function(event) {
- 	//pagination
- 	event.preventDefault();
-
- 			$('ul.flexcontainer').fadeOut('fast');
- 			$('div.loading').fadeIn('fast');
- 			//Hides the results, shows loader
-
- 			getInstagrid(instaPage);
+	 $('button#more').on('click', function(event) {
+	 	//pagination
+	 			event.preventDefault();
+				moreClicks.count++;
+				$('ul.flexcontainer').fadeOut('fast');
 
 
- 			$('div.loading').fadeOut('fast');
- 			$('ul.flexcontainer').fadeIn('slow');
 
-  });
+	 			getInstagrid(instaPage.url);
+
+				$('ul.flexcontainer').fadeIn('slow');
+
+	  });
 
 
  });
